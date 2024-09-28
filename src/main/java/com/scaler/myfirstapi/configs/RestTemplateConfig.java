1830115b6b72fa45c44df9a1aaded05786c8ef38
Vivendra Yadav <141986434+vivendra-yadav@users.noreply.github.com>
2024-09-28 21:04:02 +0530

    Update RestTemplateConfig.java

--- src/main/java/com/scaler/myfirstapi/configs/RestTemplateConfig.java
+++ src/main/java/com/scaler/myfirstapi/configs/RestTemplateConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    // we create this class to make http call
     public RestTemplate getRestTemplate() {
         return new RestTemplateBuilder().build();
     }
